9892451e9b746dc82a9a39250c0fe2a0f32eb5be
João Pedro Silva <joaopedrosilvadelima@gmail.com>
2025-07-16 20:50:04 +0000

    Refactored function to assert tests

--- hello_test.go
+++ hello_test.go
@@ -5,17 +5,20 @@ import "testing"
 func TestHello(t *testing.T) {
 	t.Run("Saying hello to people", func(t *testing.T) {
 		got := Hello("Pedro")
-		want := "Hello, Pedro"
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		want := "Hello, Pedro!"
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Say 'Hello, World' when nothing is supplyied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
