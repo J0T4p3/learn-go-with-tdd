a0a74676dafa0aac87be0346e6281079561b27c9
reversejp <joaopedrosilvadelima@gmail.com>
2025-07-26 20:01:17 -0300

    Added slices and slices tests. Slices are arrays with steroids to ease the usage, but they have caveats.

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -1,9 +1,33 @@
 package arrays
 
-func ArraySum(numbers []int) int {
+func Sum(numbers []int) int {
 	var sum int
-	for i := range len(numbers) {
+	// Defines if it will count the first value on the collection
+	for i := range numbers {
 		sum += numbers[i]
 	}
 	return sum
+}
+
+func SumAll(sumAll ...[]int) []int {
+	var sums []int
+	for _, slice := range sumAll {
+		sums = append(sums, Sum(slice))
+	}
+	return sums
+}
+
+func SumAllTails(sumAllTails ...[]int) []int {
+	var tailSums []int
+
+	for _, slice := range sumAllTails {
+		if len(slice) == 0 {
+			tailSums = append(tailSums, 0)
+		} else {
+			// Ignore the first value
+			tailSlice := slice[1:]
+			tailSums = append(tailSums, Sum(tailSlice))
+		}
+	}
+	return tailSums
 }
--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -1,12 +1,15 @@
 package arrays
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
-func TestArraySum(t *testing.T) {
+func TestSum(t *testing.T) {
 	t.Run("Adding any amount of numbers", func(t *testing.T) {
 		sum := []int{1, 2, 3, 4, 5, 1, 2, 3, 4, 5}
 
-		got := ArraySum(sum)
+		got := Sum(sum)
 		want := 30
 
 		if got != want {
@@ -17,11 +20,62 @@ func TestArraySum(t *testing.T) {
 	t.Run("Adding none numbers", func(t *testing.T) {
 		sum := []int{}
 
-		got := ArraySum(sum)
+		got := Sum(sum)
 		want := 0
 
 		if got != want {
 			t.Errorf("Got %d want %d given %v", got, want, sum)
 		}
 	})
+
+	t.Run("Adding negative numbers", func(t *testing.T) {
+		sum := []int{-1, -4}
+
+		got := Sum(sum)
+		want := -5
+
+		if got != want {
+			t.Errorf("Got %d want %d given %v", got, want, sum)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("New slice with the sum of all passed slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{2, 3})
+		want := []int{3, 5}
+
+		if !slices.Equal(got, want) {
+			t.Errorf("Got %v want %v", got, want)
+		}
+
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		if !slices.Equal(got, want) {
+			t.Helper()
+			t.Errorf("Got %v want %v", got, want)
+		}
+	}
+
+	t.Run("Sum all items of a slice except the first one", func(t *testing.T) {
+		got := SumAllTails([]int{1, 5, 6, 2}, []int{2, 3}, []int{2, 3})
+		want := []int{13, 3, 3}
+		checkSums(t, got, want)
+	})
+
+	t.Run("Sum tails of empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{}, []int{})
+		want := []int{0, 0, 0}
+		checkSums(t, got, want)
+	})
+
+	t.Run("Sum tails of unary slices", func(t *testing.T) {
+		got := SumAllTails([]int{1}, []int{3}, []int{100})
+		want := []int{0, 0, 0}
+		checkSums(t, got, want)
+	})
+
 }
