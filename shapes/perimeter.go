87237085342e99ea13894ceae489be5bb32a3a9c
reversejp <joaopedrosilvadelima@gmail.com>
2025-08-03 23:53:03 -0300

    Passed Structs lesson. Inserted new shape interface to implement Area method to all the types. Learned on how to use table tests to test multiple assertions within the same methods under the same interface on multiple types/structs.

--- shapes/perimeter.go
+++ shapes/perimeter.go
@@ -2,33 +2,36 @@ package shapes
 
 import "math"
 
+// Shape is an interface for all the methods to implement the
+// Area method. This is the GO method of setting interfaces
 type Shape interface {
 	Area() float64
 }
 
+// Types
+type Circle struct {
+	Radius float64
+}
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
-
-func (r Rectangle) Area() float64 {
-	return r.Height * r.Width
+type Triangle struct {
+	Base   float64
+	Height float64
 }
 
-type Circle struct {
-	Radius float64
-}
-
+// Methods
 func (c Circle) Area() float64 {
 	return c.Radius * c.Radius * math.Pi
 }
-
-func Perimeter(rectangle Rectangle) float64 {
-	p := 2 * rectangle.Width * rectangle.Height
-
-	return p
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height / 2
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * rectangle.Width * rectangle.Height
 }
--- shapes/perimeter_test.go
+++ shapes/perimeter_test.go
@@ -1,6 +1,9 @@
 package shapes
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
 	t.Run("Positive perimeter calculation", func(t *testing.T) {
@@ -15,22 +18,43 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("Got %.2f want %.2f", got, want)
-		}
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{
+			name:  "Rectangle()",
+			shape: Rectangle{12, 6},
+			want:  72.0,
+		},
+		{
+			name:  "Triangle()",
+			shape: Triangle{12, 6},
+			want:  36.0,
+		},
+		{
+			name:  "Circle()",
+			shape: Circle{10},
+			want:  314.1592653589793,
+		},
 	}
 
-	t.Run("Rectangles", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		checkArea(t, rectangle, 100.00)
-	})
+	for _, tt := range areaTests {
 
-	t.Run("Circles", func(t *testing.T) {
-		circle := Circle{10.0}
-		checkArea(t, circle, 314.1592653589793)
-	})
+		// Use the name to properly identify the tests
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
 
+			// Test with tolerance when using floats
+			if math.Abs(got-tt.want) > 0.0001 {
+				t.Errorf(
+					"%#v: got %g want %g",
+					tt.shape,
+					got,
+					tt.want,
+				)
+			}
+		})
+	}
 }
