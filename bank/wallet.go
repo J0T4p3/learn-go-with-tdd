679367c3d5f5971890647544170a06d2f1b00e94
reversejp <joaopedrosilvadelima@gmail.com>
2025-08-14 23:44:07 -0300

    Added more tests to Wallet struct; Created new error validations using custom types and poiters to access values; Tested other tools to show what aren't being tested on the code

--- bank/wallet.go
+++ bank/wallet.go
@@ -1,15 +1,43 @@
 package bank
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Bitcoin int
+
+func (b Bitcoin) GoString() string {
+	return fmt.Sprintf("%d BTC", b)
+}
 
 type Wallet struct {
 	balance Bitcoin
+}
+
+var ErrNegativeOrZeroTransaction = errors.New("cannot transact negative or zero values")
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.Balance() {
+		return ErrInsufficientFunds
+	}
+	if amount <= 0 {
+		return ErrNegativeOrZeroTransaction
+	}
+
+	w.balance -= amount
+	return nil
 }
 
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
-func (w *Wallet) Deposit(amount Bitcoin) {
+func (w *Wallet) Deposit(amount Bitcoin) error {
+	if amount <= 0 {
+		return ErrNegativeOrZeroTransaction
+	}
 	w.balance += amount
+	return nil
 }
--- bank/wallet_test.go
+++ bank/wallet_test.go
@@ -4,16 +4,89 @@ import (
 	"testing"
 )
 
-func TestWallet(t *testing.T) {
-	t.Run("Retrieve balance", func(t *testing.T) {
-		testAmount := Bitcoin(10)
-		wallet := Wallet{}
-		wallet.Deposit(testAmount)
+func TestWallet_deposit(t *testing.T) {
+	t.Run("Deposit positive amount", func(t *testing.T) {
+		const amount = Bitcoin(20)
+		w := Wallet{}
+		err := w.Deposit(amount)
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
-		if got != want {
-			t.Errorf("Got %d want %d", got, want)
-		}
+		assertNoError(t, err)
+		assertBalance(t, w, amount)
 	})
+
+	t.Run("Deposit zero-or-negative amount", func(t *testing.T) {
+		const negativeAmount = Bitcoin(-20)
+		const zeroAmount = Bitcoin(0)
+		const balance = Bitcoin(10)
+		w := Wallet{balance: balance}
+
+		err := w.Deposit(negativeAmount)
+		assertError(t, err, ErrNegativeOrZeroTransaction)
+		assertBalance(t, w, balance)
+
+		err = w.Deposit(zeroAmount)
+		assertError(t, err, ErrNegativeOrZeroTransaction)
+		assertBalance(t, w, balance)
+	})
+}
+
+func TestWallet_withdraw(t *testing.T) {
+	t.Run("Positive withdraw", func(t *testing.T) {
+		w := Wallet{balance: Bitcoin(20)}
+		err := w.Withdraw(Bitcoin(10))
+
+		assertNoError(t, err)
+		assertBalance(t, w, Bitcoin(10))
+	})
+
+	t.Run("Withdraw more than balance", func(t *testing.T) {
+		const balance = Bitcoin(10)
+		const withdrawAmount = Bitcoin(30)
+
+		w := Wallet{balance: balance}
+		err := w.Withdraw(withdrawAmount)
+
+		assertError(t, err, ErrInsufficientFunds)
+		assertBalance(t, w, balance)
+	})
+
+	t.Run("Withdraw negative or zero", func(t *testing.T) {
+		const zeroAmount = Bitcoin(0)
+		const negativeAmount = Bitcoin(-1)
+		const balance = Bitcoin(10)
+		w := Wallet{balance: balance}
+
+		err := w.Withdraw(zeroAmount)
+		assertError(t, err, ErrNegativeOrZeroTransaction)
+		assertBalance(t, w, balance)
+
+		err = w.Withdraw(negativeAmount)
+		assertError(t, err, ErrNegativeOrZeroTransaction)
+		assertBalance(t, w, balance)
+	})
+}
+
+func assertNoError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Errorf("got %#v want nil", err)
+	}
+}
+
+func assertBalance(t testing.TB, w Wallet, want Bitcoin) {
+	t.Helper()
+	got := w.Balance()
+	if got != want {
+		t.Errorf("got %#v want %#v", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("any errors to compare")
+	}
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
