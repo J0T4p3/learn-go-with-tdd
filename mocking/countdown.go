2cabe732c29487b23c25990f971f34d175744476
reversejp <joaopedrosilvadelima@gmail.com>
2025-08-22 19:21:18 -0300

    Included mocking. With mocked data and the usage of interfaces it's possible to pass different functions implementing the same interface methods to test the outputs of the code.

---
+++ mocking/countdown.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+// Interface who's implementing the Sleep method
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) SetDurationSlept(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+type SpyCountDownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountDownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountDownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
+func Countdown(w io.Writer, s Sleeper) {
+	const finalWord = "GO"
+	const initialCountdown = 3
+	for i := initialCountdown; i > 0; i-- {
+		fmt.Fprintf(w, "%d\n", i)
+		s.Sleep()
+		if i == 1 {
+			fmt.Fprintf(w, "GO")
+		}
+	}
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
+}
---
+++ mocking/countdown_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestCountdown(t *testing.T) {
+	t.Run("prints 3 to GO", func(t *testing.T) {
+		spyCountdown := SpyCountDownOperations{}
+		buf := bytes.Buffer{}
+		Countdown(&buf, &spyCountdown)
+
+		got := buf.String()
+		want := "3\n2\n1\nGO"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountDownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.SetDurationSlept}
+	sleeper.Sleep()
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v, slept %v", sleepTime, spyTime.durationSlept)
+	}
+
+}
